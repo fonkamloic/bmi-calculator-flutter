83f9c6631f77e15d7d0182514c8470e2993a629c
fonkamloic <fonkamloic@gmail.com>
2019-11-09 23:59:41 +0100

    file of constants

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kActiveCardColour = Color(0xff1d1e33);
+const KBottomContainerHeight = 50.0;
+const KInactiveCardColour = Color(0xff111328);
+const kBottomContainerColour = Color(0xffeb1555);
+
+const TextStyle kLableTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8d8e98),
+);
+
+const TextStyle kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
