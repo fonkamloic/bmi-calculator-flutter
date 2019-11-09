23c127cce40a015eea58ad8b7bae06f7193fd544
fonkamloic <fonkamloic@gmail.com>
2019-11-09 23:35:45 +0100

    main file

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xff011222),
+        accentColor: Color(0xff1D1F33),
+        scaffoldBackgroundColor: Color(0xff090C22),
+      ),
+      home: InputPage(),
+    );
+  }
+}
